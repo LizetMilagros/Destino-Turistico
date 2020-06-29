49ad3ccaacc1f4539a2649ce54e23459d99e4fd9
lizet milagros <lcuevas@unsa.edu.pe>
2020-06-29 04:20:10 -0500

    poniendo todos los datos en un for

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
     #return HttpResponse("<h1>hello world</h1>")
-    return render(request, 'index.html', {'price': 700})
+    dest1 =Destination()
+    dest1.name = 'Munbai'
+    dest1.desc = 'The City That Never Sleeps'
+    dest1.img = 'destination_1.jpg'
+    dest1.price = 700
+
+    dest2 =Destination()
+    dest2.name = 'Hyderabad'
+    dest2.desc = 'Firs Biryani, The Sharwari'
+    dest2.img = 'destination_2.jpg'
+    dest2.price = 650
+
+    dest3 =Destination()
+    dest3.name = 'Bangaluru'
+    dest3.desc = 'Beautiful city'
+    dest3.img = 'destination_3.jpg'
+    dest3.price = 750
+
+    dests = [dest1, dest2, dest3]
+    return render(request, 'index.html', {'dests': dests})

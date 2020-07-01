276bc844c22efde7f555d26f788ebe8cb9c1333b
lizet milagros <lcuevas@unsa.edu.pe>
2020-07-01 13:54:42 -0500

    ingresando los datos a TRAVELLO

--- travello/views.py
+++ travello/views.py
@@ -2,27 +2,8 @@ from django.shortcuts import render
 from .models import Destination
 # Create your views here.
 def index(request):
-    #return HttpResponse("<h1>hello world</h1>")
-    dest1 =Destination()
-    dest1.name = 'Munbai'
-    dest1.desc = 'The City That Never Sleeps'
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 700
-    dest1.offer = False
 
-    dest2 =Destination()
-    dest2.name = 'Hyderabad'
-    dest2.desc = 'Firs Biryani, The Sharwari'
-    dest2.img = 'destination_2.jpg'
-    dest2.price = 650
-    dest2.offer =True
+    dests =Destination.objects.all()
 
-    dest3 =Destination()
-    dest3.name = 'Bangaluru'
-    dest3.desc = 'Beautiful city'
-    dest3.img = 'destination_3.jpg'
-    dest3.price = 750
-    dest3.offer = False
 
-    dests = [dest1, dest2, dest3]
     return render(request, 'index.html', {'dests': dests})

565fb5908aa1b2de9e785675b87fd7a3ac575c83
lizet milagros <lcuevas@unsa.edu.pe>
2020-07-09 02:20:17 -0500

    agregando los path de eliminar y editar

--- travello/urls.py
+++ travello/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('lista', views.lista, name = 'lista'),
-    path('añadir', views.añadir, name= 'añadir')
+    path('añadir', views.añadir, name= 'añadir'),
+    path('editar/<int:id>/', views.editar, name= 'editar'),
+    #path('eliminar/<int:id>/', views.eliminar, name= 'eliminar'),
 
 ]
